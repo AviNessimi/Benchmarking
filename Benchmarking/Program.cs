aa9cc59cd7a27fb495a1ae409f4a831e976e3156
Avi Nessimian <Avi.Nessimian@checkmarx.com>
2022-05-23 14:11:55 +0300

    Add project files.

---
+++ Benchmarking/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+using Benchmarking;
+
+// Run benchmarking on all types in the specified assembly
+//var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+
+// Run benchmarking on the specified type
+var summary = BenchmarkRunner.Run<Test1>();
