db17081ac808bb1f6c3bcf8da20cc3a4ec27fb2e
Avi Nessimian <Avi.Nessimian@checkmarx.com>
2022-06-07 22:24:17 +0300

    web test

---
+++ WebApplication1/Controllers/Loggers.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+using Serilog.Events;
+
+
+namespace WebApplication1.Controllers
+{
+    public static class Loggers
+    {
+
+        public static Serilog.ILogger ConsoleLogger = new LoggerConfiguration()
+            .MinimumLevel.Is(LogEventLevel.Information)
+        .WriteTo.Console()
+        .CreateLogger();
+
+        public static Serilog.ILogger AsyncConsoleLogger = new LoggerConfiguration()
+            .MinimumLevel.Is(LogEventLevel.Information)
+        .WriteTo.Async(writeTo => writeTo.Console())
+        .CreateLogger();
+
+    }
+}
