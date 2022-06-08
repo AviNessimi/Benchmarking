cf4205b4e609b9f717add964a4eec7f3d03cc4ca
Avi Nessimian <Avi.Nessimian@checkmarx.com>
2022-06-09 01:53:19 +0300

    s

--- WebApp/WebApplication1/Controllers/SerilogConsoleLoggerController.cs
+++ WebApp/WebApplication1/Controllers/SerilogConsoleLoggerController.cs
@@ -30,5 +30,31 @@ namespace WebApplication1.Controllers
                 logger.Error(ex, msg);
             }
         }
+
+
+
+        [HttpGet("threadCount")]
+        public ThreadCount Get()
+        {
+            ThreadCount threadCount = new();
+            ThreadPool.GetMinThreads(out int workerThreads, out int completionPortThreads);
+            threadCount.MinWorkerThreads = workerThreads;
+            threadCount.MinCompletionPortThreads = completionPortThreads;
+
+            ThreadPool.GetMaxThreads(out workerThreads, out completionPortThreads);
+            threadCount.MaxWorkerThreads = workerThreads;
+            threadCount.MaxCompletionPortThreads = completionPortThreads;
+
+            return threadCount;
+        }
+
+
+        public class ThreadCount
+        {
+            public int MinWorkerThreads { get; set; }
+            public int MinCompletionPortThreads { get; set; }
+            public int MaxWorkerThreads { get; set; }
+            public int MaxCompletionPortThreads { get; set; }
+        }
     }
 }
--- WebApp/WebApplication1/Program.cs
+++ WebApp/WebApplication1/Program.cs
@@ -3,6 +3,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+ThreadPool.SetMinThreads(400, 400);
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
